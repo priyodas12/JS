44c6a35695ffc3a2c6785b0217d7c9dc67353471
Priyobrato.Das <priyodas12@gmail.com>
2022-12-12 19:08:30 +0530

    math

---
+++ src_js/app_6_math.js
@@ -0,0 +1,20 @@
+console.log(Math.max(2,4,15,0,1));
+
+console.log(Math.min(2,-4,5,0,1));
+
+console.log(Math.round(5.79929));//6
+
+console.log(Math.floor(5.92929));//5
+
+console.log(Math.pow(4, 6));//4096
+
+console.log(Math.imul(3, 8))//3
+
+console.log(Math.random());
+
+let a = Math.random();
+
+console.log(a);
+console.log(Math.floor(a * 1000));
+
+console.log(Math.round(a * 1000));
