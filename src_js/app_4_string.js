64d9147868344711c2f7d39af4ed520728c97cfb
Priyobrato.Das <priyodas12@gmail.com>
2022-12-12 17:45:35 +0530

    indexOf function in string

--- src_js/app_4_string.js
+++ src_js/app_4_string.js
@@ -4,3 +4,10 @@ console.log(complexName);
 var foo="foo";
 var joe=foo+"joe";
 
+let myTeam = "Argentina";
+
+//indexOf() operation case sensitive;
+console.log(myTeam.indexOf('t'));//5
+console.log(myTeam.indexOf('gen'));//2
+console.log(myTeam.indexOf('o')); // -1
+
