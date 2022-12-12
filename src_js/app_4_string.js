3894947bcfd99df4bb46f8915fbc7d0fd1207af8
Priyobrato.Das <priyodas12@gmail.com>
2022-12-12 18:55:25 +0530

    null and undefined values.

--- src_js/app_4_string.js
+++ src_js/app_4_string.js
@@ -15,5 +15,39 @@ let myWcTeam = "Morocco";
 
 //str[x]= x will be inclusive;
 console.log(myWcTeam.slice(3));
-console.log(myWcTeam.slice(4,6));
+console.log(myWcTeam.slice(4, 6));
+
+let expressionOnFb = "Football is my favorite game!";
+
+// string must be matched!
+let changedExpression=expressionOnFb.replace("Football", "Cricket");
+
+console.log(expressionOnFb);
+console.log(changedExpression);
+
+//-------------------String special syambol-------------------------
+
+let testSentence1 = "hey there \n I am using \n whatsapp!";
+
+console.log(testSentence1);
+
+let testSentence2 = "hey there \' I \' am using \t whatsapp!";
+
+console.log(testSentence2);
+
+let testSentence3 = "hey there \\ I \\ am using \\ whatsapp!";
+
+console.log(testSentence3);
+
+let testSentence4 = "hey there \" I \" am using \t whatsapp!";
+
+console.log(testSentence4);
+
+//----------------------String Template Literals----------------------
+let counterString = `I have counted ${4 * 5} time today: ${new Date()}`;
+console.log(counterString);
+
+let userName = "priyobrato";
+console.log(`${userName.toUpperCase()}`)
+
 
---
+++ src_js/app_5_null_and_undefined.js
@@ -0,0 +1,17 @@
+//Intentional absence of any value!
+let myName = null;
+
+console.log(myName);
+
+myName = "priyo";
+
+console.log(myName);
+
+//Variable that does not have an assigned value!
+let mySurName;
+
+console.log(mySurName);
+
+mySurName = "das";
+
+console.log(mySurName);
