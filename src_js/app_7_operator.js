7dab00c5f5226ac6e82eaf80a25ad637420e45df
Priyobrato.Das <priyodas12@gmail.com>
2022-12-13 21:54:46 +0530

    operators

---
+++ src_js/app_7_operator.js
@@ -0,0 +1,16 @@
+console.log(2 == '2');
+
+console.log(0 == false);
+
+console.log(0 == '');
+
+console.log(4 == 4)
+//above all prints true;
+
+console.log(4 === '4');
+
+console.log(0 === false);
+
+console.log(0 === '');
+
+//above all prints false;
