fe5062464d2a962ec919d5b070c2565e3b60e5a2
Priyobrato.Das <priyodas12@gmail.com>
2022-12-21 22:36:21 +0530

    sort

--- src_js/array/app_5_array_sort.js
+++ src_js/array/app_5_array_sort.js
@@ -1,6 +1,11 @@
 let s1 = ["abc", "xys", "wef", "ave", "deg", "lbn"];
-
+//defautlt sort is string character sort.
 console.log(s1);//  ['abc', 'xys', 'wef', 'ave', 'deg', 'lbn']
 s1.sort();
 console.log(s1);//  ['abc', 'ave', 'deg', 'lbn', 'wef', 'xys']
+
+let n1 = [54000, 12111, 111, 321, 14, 1000000];
+
+n1.sort();
+console.log(n1);//[1000000, 111, 12111, 14, 321, 54000]=> its started sorting with 1st character.
 
