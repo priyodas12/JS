5eb7c274f9e0cc85b1a0835a60aaa19bd4f9695c
Priyobrato.Das <priyodas12@gmail.com>
2022-12-21 22:12:09 +0530

    sorting

---
+++ src_js/array/app_5_array_sort.js
@@ -0,0 +1,6 @@
+let s1 = ["abc", "xys", "wef", "ave", "deg", "lbn"];
+
+console.log(s1);//  ['abc', 'xys', 'wef', 'ave', 'deg', 'lbn']
+s1.sort();
+console.log(s1);//  ['abc', 'ave', 'deg', 'lbn', 'wef', 'xys']
+
