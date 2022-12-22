09d559f44f809a423c14fb5341844238ddf66585
Priyobrato.Das <priyodas12@gmail.com>
2022-12-22 22:50:55 +0530

    objects

---
+++ src_js/object/1_app_obj_intro.js
@@ -0,0 +1,13 @@
+let fitnessData = {
+    stepCountDaily: 3000,
+    stepCountWeekly: 89000,
+    heartBeat: "82bpm",
+    spo2: "98%",
+    avgCalBurn: "350Kcal",
+    temp: 36.8,
+    id: 2345,
+    23:" twenty three "
+}
+
+console.log(fitnessData[23]);
+console.log(fitnessData);
