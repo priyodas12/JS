6a54ef1193a011e337233fc7e9d615f706989d7c
Priyobrato.Das <priyodas12@gmail.com>
2022-12-24 11:16:02 +0530

    forEach loop with index.

--- src_js/functions/6_app_func_array.js
+++ src_js/functions/6_app_func_array.js
@@ -1,7 +1,7 @@
 const numbers = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
 
-numbers.forEach(function (num) { 
-    console.log(num * num);
+numbers.forEach(function (index,num) { 
+    console.log(index,num * num);
 })
 
 console.log("**************** by arrow funtions **********")
