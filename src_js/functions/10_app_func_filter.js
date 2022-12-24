f9a363a038800bb05762cc2de6c14b110b180318
Priyobrato.Das <priyodas12@gmail.com>
2022-12-24 14:02:57 +0530

    filter functions

---
+++ src_js/functions/10_app_func_filter.js
@@ -0,0 +1,8 @@
+const numbers = [12, 33, 71, 38, 91, 82, 73];
+
+const evenNums = numbers.filter((n) => n % 2 === 0);
+
+const oddNums = numbers.filter((n) => n % 2 === 1);
+
+console.log(evenNums);
+console.log(oddNums);
