96eafd1cb0520acbaebadf22c99a265799150ea4
Priyobrato.Das <priyodas12@gmail.com>
2022-12-24 22:44:54 +0530

    param destructure

---
+++ src_js/features/7_app_param_destructure.js
@@ -0,0 +1,11 @@
+const runner = {
+  name: "robert",
+  country: "Jemica",
+  timeTaken: 9.2,
+};
+
+const performace = ({ name, timeTaken }) => {
+  console.log(`${name} ${timeTaken}`);
+};
+
+performace(runner);
