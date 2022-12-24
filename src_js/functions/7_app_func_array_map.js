d9e7c09ec31172de7cae9e9287a0b0b1df3a3c7e
Priyobrato.Das <priyodas12@gmail.com>
2022-12-24 12:10:50 +0530

    map functions

---
+++ src_js/functions/7_app_func_array_map.js
@@ -0,0 +1,24 @@
+//create new array from an existing array.
+
+const numbers = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+
+const doubledNumbers = numbers.map(function (num) {
+  return num * 2;
+});
+
+const evenOrOddNumbers = numbers.map(function (n) {
+  return {
+    number: n,
+    isEven: n % 2 == 0,
+  };
+});
+console.log(evenOrOddNumbers);
+console.log(doubledNumbers);
+
+const words = ["abc", "yr", "wrt", "dgt", "cvf"];
+
+const upperWords = words.map(function (w) {
+  return w.toLocaleUpperCase().split("").join(".");
+});
+
+console.log(upperWords);
