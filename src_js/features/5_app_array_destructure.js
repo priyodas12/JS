2ef1ff6747bae7e872f4ba07b22984bfd6d4036e
Priyobrato.Das <priyodas12@gmail.com>
2022-12-24 22:20:49 +0530

    array destructruing

---
+++ src_js/features/5_app_array_destructure.js
@@ -0,0 +1,10 @@
+const names = ["Robin", "Makou", "Devis", "Mike", "Tommas"];
+
+//before array destructuring
+// const gold = names[0];
+// const silver = names[1];
+// const bronze = names[2];
+
+const [gold, silver, bronze] = names;
+
+console.log(gold, silver, bronze);
