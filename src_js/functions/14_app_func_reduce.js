29070112ece9e87b6554f0ab31f8f086e2f619af
Priyobrato.Das <priyodas12@gmail.com>
2022-12-24 14:58:53 +0530

    reduce

--- src_js/functions/14_app_func_reduce.js
+++ src_js/functions/14_app_func_reduce.js
@@ -1,3 +1,4 @@
+// sum of all elemnts of an array
 const numbers = [12, 33, 71, 38, 91, 82, 73];
 
 let sumOfNumbers = numbers.reduce((accumulator, currentValue) => {
@@ -7,9 +8,35 @@ let sumOfNumbers = numbers.reduce((accumulator, currentValue) => {
 
 console.log(sumOfNumbers);
 
+/* here 0 indicates initial value if that is not provided it will consider first elements
+let sumOfNumbers = numbers.reduce((accumulator, currentValue) => {
+  console.log(accumulator, currentValue);
+  return accumulator + currentValue;
+},0);
+
+console.log(sumOfNumbers);
+*/
+
+// multiplication value of all array elemnts.
 const allNums = [1, 2, 3, 4, 5, 6, 7, 8, 9];
 
 let sumOfFibboNumbers = allNums.reduce((accumulator, currentValue) => {
   console.log(accumulator, currentValue);
   return accumulator * currentValue;
 });
+
+//finding max elemnts from an array.
+let maxNumber = numbers.reduce((max, currectValue) => {
+  //   if (currectValue > max) return currectValue;
+  //   return max;
+  return Math.max(max, currectValue);
+});
+
+let minNumber = numbers.reduce((max, currectValue) => {
+  //   if (currectValue > max) return currectValue;
+  //   return max;
+  return Math.min(max, currectValue);
+});
+
+console.log(`MaxNumber: : ${maxNumber}`);
+console.log(`MinNumber: : ${minNumber}`);
