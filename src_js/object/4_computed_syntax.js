d6f45c7e553cebe3644014692168fa1b8d1cb622
Priyobrato.Das <priyodas12@gmail.com>
2022-12-27 00:30:55 +0530

    computed properties

---
+++ src_js/object/4_computed_syntax.js
@@ -0,0 +1,10 @@
+const role1 = "Developer";
+const person1 = "Rohom";
+const role2 = "Devops";
+const person2 = "James";
+
+const team = {};
+team[person1] = role1;
+team[person2] = role2;
+
+console.log(team);
