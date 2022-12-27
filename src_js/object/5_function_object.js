74c166beca6b4011397957a4584f5349ade6e91b
Priyobrato.Das <priyodas12@gmail.com>
2022-12-27 13:02:48 +0530

    function in object

---
+++ src_js/object/5_function_object.js
@@ -0,0 +1,12 @@
+const math = {
+  add: function (a, b) {
+    return a + b;
+  },
+  devide: function (a, b) {
+    return a - b;
+  },
+  multiply: function (a, b) {
+    return a * b;
+  },
+};
+console.log(math.add(20, 23));
