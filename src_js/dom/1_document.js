395574e2b7c1a87a1d97c928d65c93bab664e1a3
Priyobrato.Das <priyodas12@gmail.com>
2022-12-29 11:28:18 +0530

    innerText

--- src_js/dom/1_document.js
+++ src_js/dom/1_document.js
@@ -1,3 +1,9 @@
 console.log(document);
 let pContext1 = document.getElementById("main");
 console.log(pContext1);
+
+let tagName1 = document.getElementsByTagName("input");
+console.log(tagName1); //get as HTMLColection
+
+let queryTagName = document.querySelectorAll("input");
+console.log(queryTagName); //get as NodeList
---
+++ src_js/dom/2_document_methods.js
@@ -0,0 +1,26 @@
+let h1 = document.querySelector("h1");
+
+console.log(h1);
+
+let h1InnerText = h1.innerText;
+//text between h1 tag
+console.log(h1InnerText);
+
+h1InnerText = "Testing it!"; //it will just update the string value of variable
+//h1.innerText = h1InnerText; //here it goes with update!
+
+const ul = document.querySelector("ul");
+
+console.log(ul);
+
+const ulInnerText = ul.innerText;
+
+console.log(ulInnerText); // it will represents all text as list inside li tag.
+
+//get all text inside the web page
+
+const body = document.body;
+
+const bodyText = body.innerText;
+
+console.log(bodyText);
