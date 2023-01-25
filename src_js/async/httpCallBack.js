b7738d38c04cf1d6fa80100e71b1ff15ddd37cea
Priyobrato.Das <priyodas12@gmail.com>
2023-01-25 10:31:16 +0530

    callback impl2

--- src_js/async/httpCallBack.js
+++ src_js/async/httpCallBack.js
@@ -1,9 +1,22 @@
-let request = new XMLHttpRequest();
+let assignTodos = (callback) => {
+  let request = new XMLHttpRequest();
 
-request.addEventListener("readystatechange", () => {
-  console.log(request, request.readyState);
+  request.addEventListener("readystatechange", () => {
+    if (request.readyState === 4 && request.status === 200) {
+      //console.log(request, request.readyState);
+      callback(undefined, request.response);
+    } else if (request.readyState === 4) {
+      //console.log("can't able to fetch!");
+      callback(request.status, undefined);
+    }
+  });
+
+  request.open("GET", "https://jsonplaceholder.typicode.com/todoss");
+
+  request.send();
+};
+
+assignTodos((err, data) => {
+  console.log("callback triggered!");
+  console.log(err, data);
 });
-
-request.open("GET", "https://jsonplaceholder.typicode.com/todos");
-
-request.send();
