541ec1a874a1a076eff5bd425fcab891d0894186
Priyobrato.Das <priyodas12@gmail.com>
2023-01-25 10:16:50 +0530

    callback

--- src_js/async/callbackImpl.js
+++ src_js/async/callbackImpl.js
@@ -1,5 +1,5 @@
 console.log("first!");
 setTimeout(() => {
-  console.log("third!!!");
+  console.log("third!!!---executed by browser, JS has not capability here!");
 }, 3000);
 console.log("second!!");
---
+++ src_js/async/httpCallBack.js
@@ -0,0 +1,9 @@
+let request = new XMLHttpRequest();
+
+request.addEventListener("readystatechange", () => {
+  console.log(request, request.readyState);
+});
+
+request.open("GET", "https://jsonplaceholder.typicode.com/todos");
+
+request.send();
